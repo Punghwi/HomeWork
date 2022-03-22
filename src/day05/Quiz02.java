4641337c4ed2dfa63688be5aa4a6efc7723291e7
Punghwi <white487@naver.com>
2022-03-22 19:53:35 +0900

    집에서 숙제 마무리

--- src/day05/Quiz02.java
+++ src/day05/Quiz02.java
@@ -22,6 +22,17 @@ public class Quiz02 {
 //			}
 //		}
 		
+		
+		//3번 문제
+//		for(int i=1; i<=25; i++) {
+//			System.out.print(i + "\t");
+//			if(i%5==0) {
+//				System.out.println();
+//			}
+//		}
+		
+		
+		
 		//4번 문제
 //		int sum=1;
 //		
@@ -73,17 +84,17 @@ public class Quiz02 {
 		//한마리 하루 20g 먹음
 		// day%10==0 -> 쥐*2
 		
-		int day,rice,mouse = 2;
-		rice = 100000;
-		
-		for(day=1; rice>0; day++) {
-			rice -= mouse*20;
-			if(day%10==0) {
-				mouse*=2;
-			}
-		}
-		day--;
-		System.out.println(day + "일 " + mouse + "마리 " + rice + "g남음");
+//		int day,rice,mouse = 2;
+//		rice = 100000;
+//		
+//		for(day=1; rice>0; day++) {
+//			rice -= mouse*20;
+//			if(day%10==0) {
+//				mouse*=2;
+//			}
+//		}
+//		day--;
+//		System.out.println(day + "일 " + mouse + "마리 " + rice + "g남음");
 		
 		
 		
@@ -92,16 +103,60 @@ public class Quiz02 {
 		// 최소공배수 = 공배수 중에서 가장 작은 정수
 		// 최대공약수 = 공약수 중에서 가장 큰 수
 		
+//		int su1, su2, min=0, num, pl=1;
+//		System.out.print("수 입력 : ");
+//		su1 = sc.nextInt();
+//		System.out.print("수 입력 : ");
+//		su2 = sc.nextInt();
+//		for(int i=1;i<=su1 && i<=su2;i++){
+//			if(su1%i==0 && su2%i==0)
+//				min=i;
+//		}
+//		//max=num1*num2/min;	//최소 공배수 구하는 식
+//		System.out.println("최대공약수 : " + min);
+//		if(su1>su2) {
+//			num = su1;
+//		}else {
+//			num = su2;
+//		}
+//		for(int i=num; pl<2; i++) {
+//			if(i%su1==0 && i%su2==0) {
+//				System.out.println("최소공배수 : " + i);
+//				pl++;
+//			}
+//		}
+
+		
+		
 		//9번 문제
 		//어떤 수의 약수를 구하는 프로그램을 작성하여라
 		// 약수 = 어떤 수를 정수로 나눌 수 있는 수
 		
+//		int i,su;
+//		su = 0;
+//		System.out.print("수를 입력하세요 : ");
+//		su = sc.nextInt();
+//		
+//		for(i=1;i<=su;i++) {
+//			if(su%i==0) {
+//				System.out.println("약수 : " + i);
+//			}		
+//		}
+		
 		//10번 문제
 		//1에서 1000까지의 자연수중에서 4로 나누어도 6으로 나누어도 나머지가 1인 수의 합을 출력하라
 		
-		
-		
-		
+//		int i,sum=0;
+//		for(i=1; i<=1000; i++) {
+//			if(i%4==1 && i%6==1) {
+//				sum+=i;
+//				System.out.println();
+//				System.out.println("4,6으로 나누어도 나머지 1인 수 : " + i);
+//				System.out.println("4,6으로 나누어도 나머지 1인 수의 합 : " + sum);
+//			}
+//		}
+//		System.out.println("4,6으로 나누어도 나머지 1인 수의 합 : " + sum);
+			
 		
 	}
 }

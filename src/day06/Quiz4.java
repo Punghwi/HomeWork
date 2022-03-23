d34c8ed863c6063b037f6081fbcb5c694609a707
Punghwi <white487@naver.com>
2022-03-23 19:14:25 +0900

    집에서숙제완료_수요일

--- src/day06/Quiz4.java
+++ src/day06/Quiz4.java
@@ -1,7 +1,11 @@
 package day06;
 
+import java.util.Scanner;
+
 public class Quiz4 {
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 //		for(int i=1;i<=5;i++) {
 //			for(int j=1;j<=i;j++) {
@@ -10,18 +14,39 @@ public class Quiz4 {
 //			System.out.println();
 //		}
 		
-		for(int i=1; i<=5;i++) {
-			for(int j=0;j<=5-i;j++) {
-			System.out.print("*");
+
+		int n = 4;
+		
+		for (int i=1; i<=5; i++){
+			for(int j=n; j>0; j--){
+				System.out.print(" ");
+			}
+			for(int k=i; k>0; k--){
+				System.out.print("*");
 			}
 			System.out.println();
+			n--;
 		}
 		
+		int i,j=0;
+	      
+	    for(i=5; i>0; i--) {
+	       for(j=1; j<=i-1; j++) {
+	          System.out.print(" ");
+	       }
+	       for(j=1; j<=6-i; j++) {
+	          System.out.print("*");
+	       }
+	       System.out.println();
+	    }
 		
+	    System.out.println("    *");
+	    System.out.println("   **");
+	    System.out.println("  ***");
+	    System.out.println(" ****");
+	    System.out.println("*****");
 		
-		
-		
-		
+
 		
 	}
 }
